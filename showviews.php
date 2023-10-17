39f8c468a375e872675567a3b7f9acc6a2278e2b
Ladislav Gálik <821786+vlgalik@users.noreply.github.com>
2023-10-17 16:50:57 +0200

    Create showviews.php

---
+++ showviews.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * ShowViews plugin for Adminer Editor.
+ *
+ * @link https://github.com/vlgalik/adminer-showviews
+ * @author Ladislav Galik, https://galik.it
+ */
+
+class AdminerShowViews
+{
+    function tablesPrint(&$tables)
+    {
+        if (!preg_match("/editor/i", Adminer::name())) return;
+
+        foreach ($tables as $table => $status) {
+            $tables[$table]['Engine'] = '';
+        }
+    }
+}
